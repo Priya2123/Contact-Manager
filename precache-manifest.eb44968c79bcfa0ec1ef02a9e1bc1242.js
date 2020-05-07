3bcde0610d4fc5aeb55188fb31644b5f31cef65b
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-05-07 16:25:12 +0530

    Updates

---
+++ precache-manifest.eb44968c79bcfa0ec1ef02a9e1bc1242.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a613bd7fc2a9f7f961dee95a9630b28d",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "65320b9081ca88a8fb6f",
+    "url": "/Contact-Manager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "681c15a5abf5c99b481a",
+    "url": "/Contact-Manager/static/css/main.ec24ccd9.chunk.css"
+  },
+  {
+    "revision": "65320b9081ca88a8fb6f",
+    "url": "/Contact-Manager/static/js/2.30480e19.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager/static/js/2.30480e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681c15a5abf5c99b481a",
+    "url": "/Contact-Manager/static/js/main.5a0a2008.chunk.js"
+  },
+  {
+    "revision": "067516ef4395b5180222",
+    "url": "/Contact-Manager/static/js/runtime-main.82a14529.js"
+  }
+]);
