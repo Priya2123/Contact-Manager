653c1deee41690694acb2575cc74a40b291b5327
Priya2123 <aggarwalpriya1121@gmail.com>
2022-10-06 17:12:35 +0530

    wip

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
-    return (
-        <div>
-            <h1 className="display-4">About Contacts</h1>
-            <p className="lead">Simple app to manage Contacts</p>
-            <p className="text-secondary">Version 1.0.0</p>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1 className="display-4">About Contacts</h1>
+      <p className="lead">Simple app to manage Contacts</p>
+      <p className="text-secondary">Version 1.0.0</p>
+      <br />
+      <p>
+        Contact management is a means of entering customer and lead data for
+        storage, quick access, editing and tracking. Contact management can be
+        as simple as a spreadsheet or as complex as customer relationship
+        management (CRM) software.
+        <br />
+        <br />
+        By digitizing contact management, you can give your employees access to
+        valuable customer data. All departments can view the same data, so you
+        speed up communication and streamline processes. When you make
+        communication more efficient, you can focus less on business tasks and
+        more on your customers.
+        <br />
+        <br />
+        In today’s business environment, you have to do more than just
+        personalize a sales email with someone’s name in order to make a sale.
+        Customers are loyal to companies that remember their specific
+        preferences, and they expect a stellar experience every time they place
+        an order online or receive a package in the mail. Going above and beyond
+        not only creates repeat sales but also encourages referrals, which could
+        significantly decrease your overall customer acquisition costs.
+      </p>
+    </div>
+  );
+}
